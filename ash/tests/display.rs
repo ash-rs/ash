6738c4c01c16e480ad785b5d7ccddca4d2e165c4
Benjamin Saunders <ben.e.saunders@gmail.com>
2018-08-19 19:40:28 -0700

    impl Display for constants

---
+++ ash/tests/display.rs
@@ -0,0 +1,12 @@
+extern crate ash;
+use ash::vk;
+
+#[test]
+fn display_flags() {
+    assert_eq!((vk::AccessFlags::INDIRECT_COMMAND_READ | vk::AccessFlags::VERTEX_ATTRIBUTE_READ).to_string(), "INDIRECT_COMMAND_READ | VERTEX_ATTRIBUTE_READ");
+}
+
+#[test]
+fn display_enum() {
+    assert_eq!(vk::ChromaLocation::MIDPOINT.to_string(), "MIDPOINT");
+}
