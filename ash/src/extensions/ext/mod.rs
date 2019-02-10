011e9e611fa9ea79f23b9cb8a5b48c2fc1eb040d
Graham Wihlidal <graham@wihlidal.ca>
2019-02-10 23:54:44 +0100

    Added DescriptorIndexing extension (just name for now), and also added names for VK_KHR_get_memory_requirements2 and VK_KHR_get_physical_device_properties2 (both are commonly used, and required for ray tracing)

--- ash/src/extensions/ext/mod.rs
+++ ash/src/extensions/ext/mod.rs
@@ -1,7 +1,19 @@
 pub use self::debug_marker::DebugMarker;
 pub use self::debug_report::DebugReport;
 pub use self::debug_utils::DebugUtils;
+pub use self::descriptor_indexing::DescriptorIndexing;
 
 mod debug_marker;
 mod debug_report;
 mod debug_utils;
+mod descriptor_indexing;
+
+pub fn memory_requirements2_name() -> &'static std::ffi::CStr {
+    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_get_memory_requirements2\0")
+        .expect("Wrong extension string")
+}
+
+pub fn physical_device_properties2_name() -> &'static std::ffi::CStr {
+    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_get_physical_device_properties2\0")
+        .expect("Wrong extension string")
+}
