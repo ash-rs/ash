09ee78401fab767529b46cecadf34e53145214bc
Graham Wihlidal <graham@wihlidal.ca>
2019-02-11 15:43:49 +0100

    Added maintenance 1-3 extension names to khr/mod.rs and moved KHR extension names from EXT to proper place.

--- ash/src/extensions/ext/mod.rs
+++ ash/src/extensions/ext/mod.rs
@@ -8,12 +8,3 @@ mod debug_report;
 mod debug_utils;
 mod descriptor_indexing;
 
-pub fn memory_requirements2_name() -> &'static std::ffi::CStr {
-    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_get_memory_requirements2\0")
-        .expect("Wrong extension string")
-}
-
-pub fn physical_device_properties2_name() -> &'static std::ffi::CStr {
-    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_get_physical_device_properties2\0")
-        .expect("Wrong extension string")
-}
--- ash/src/extensions/khr/mod.rs
+++ ash/src/extensions/khr/mod.rs
@@ -15,3 +15,28 @@ mod wayland_surface;
 mod win32_surface;
 mod xcb_surface;
 mod xlib_surface;
+
+pub fn memory_requirements2_name() -> &'static std::ffi::CStr {
+    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_get_memory_requirements2\0")
+        .expect("Wrong extension string")
+}
+
+pub fn physical_device_properties2_name() -> &'static std::ffi::CStr {
+    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_get_physical_device_properties2\0")
+        .expect("Wrong extension string")
+}
+
+pub fn maintenance1_name() -> &'static std::ffi::CStr {
+    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_maintenance1\0")
+        .expect("Wrong extension string")
+}
+
+pub fn maintenance2_name() -> &'static std::ffi::CStr {
+    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_maintenance2\0")
+        .expect("Wrong extension string")
+}
+
+pub fn maintenance3_name() -> &'static std::ffi::CStr {
+    std::ffi::CStr::from_bytes_with_nul(b"VK_KHR_maintenance3\0")
+        .expect("Wrong extension string")
+}
