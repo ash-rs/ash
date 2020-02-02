651462a342b7c835c3dbebe0428ec13d68407e45
Benjamin Saunders <ben.e.saunders@gmail.com>
2020-02-02 01:42:59 -0800

    Fix XCB types (#267)

--- ash/src/vk.rs
+++ ash/src/vk.rs
@@ -45,11 +45,11 @@ pub type VisualID = c_uint;
 pub type Display = *const c_void;
 pub type Window = c_ulong;
 #[allow(non_camel_case_types)]
-pub type xcb_connection_t = *const c_void;
+pub type xcb_connection_t = c_void;
 #[allow(non_camel_case_types)]
 pub type xcb_window_t = u32;
 #[allow(non_camel_case_types)]
-pub type xcb_visualid_t = *const c_void;
+pub type xcb_visualid_t = u32;
 pub type MirConnection = *const c_void;
 pub type MirSurface = *const c_void;
 pub type HINSTANCE = *const c_void;
