7b009dc20b75582aa477fb5483ea7c157b9daaf9
msiglreith <m.siglreith@gmail.com>
2020-07-03 19:10:17 +0200

    Add ash-window as workspace member

---
+++ ash-window/examples/beryllium.rs
@@ -0,0 +1,56 @@
+//! Demonstrate interop with beryllium/SDL windows.
+//!
+//! Sample creates a surface from a window through the
+//! platform agnostic window handle trait.
+//!
+//! On instance extensions platform specific extensions need to be enabled.
+
+use ash::{version::EntryV1_0, vk};
+use beryllium::*;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let sdl = beryllium::init()?;
+
+    let window = sdl.create_window(
+        "ash-window x beryllium",
+        WINDOW_POSITION_CENTERED,
+        WINDOW_POSITION_CENTERED,
+        800,
+        600,
+        WindowFlags::default(),
+    )?;
+
+    unsafe {
+        let entry = ash::Entry::new()?;
+        let surface_extensions = ash_window::enumerate_required_extensions(&window)?;
+        let instance_extensions = surface_extensions
+            .iter()
+            .map(|ext| ext.as_ptr())
+            .collect::<Vec<_>>();
+        let app_desc = vk::ApplicationInfo::builder().api_version(vk::make_version(1, 0, 0));
+        let instance_desc = vk::InstanceCreateInfo::builder()
+            .application_info(&app_desc)
+            .enabled_extension_names(&instance_extensions);
+
+        let instance = entry.create_instance(&instance_desc, None)?;
+
+        // Create a surface from winit window.
+        let surface = ash_window::create_surface(&entry, &instance, &window, None)?;
+        let surface_fn = ash::extensions::khr::Surface::new(&entry, &instance);
+        println!("surface: {:?}", surface);
+
+        'main: loop {
+            while let Some(event) = sdl.poll_event() {
+                match event {
+                    Event::Quit { .. } => break 'main,
+                    _ => (),
+                }
+            }
+        }
+
+        surface_fn.destroy_surface(surface, None);
+    }
+
+    Ok(())
+}
---
+++ ash-window/src/lib.rs
@@ -0,0 +1,171 @@
+use ash::{
+    extensions::khr,
+    prelude::*,
+    version::{EntryV1_0, InstanceV1_0},
+    vk,
+};
+use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
+use std::ffi::CStr;
+
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+use ash::extensions::ext; // portability extensions
+
+/// Create a surface from a raw surface handle.
+///
+/// `instance` must have created with platform specific surface extensions enabled.
+pub unsafe fn create_surface<E, I>(
+    entry: &E,
+    instance: &I,
+    window_handle: &dyn HasRawWindowHandle,
+    allocation_callbacks: Option<&vk::AllocationCallbacks>,
+) -> VkResult<vk::SurfaceKHR>
+where
+    E: EntryV1_0,
+    I: InstanceV1_0,
+{
+    match window_handle.raw_window_handle() {
+        #[cfg(target_os = "windows")]
+        RawWindowHandle::Windows(handle) => {
+            let surface_desc = vk::Win32SurfaceCreateInfoKHR::builder()
+                .hinstance(handle.hinstance)
+                .hwnd(handle.hwnd);
+            let surface_fn = khr::Win32Surface::new(entry, instance);
+            surface_fn.create_win32_surface(&surface_desc, allocation_callbacks)
+        }
+
+        #[cfg(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd",
+            target_os = "openbsd"
+        ))]
+        RawWindowHandle::Wayland(handle) => {
+            let surface_desc = vk::WaylandSurfaceCreateInfoKHR::builder()
+                .display(handle.display)
+                .surface(handle.surface);
+            let surface_fn = khr::WaylandSurface::new(entry, instance);
+            surface_fn.create_wayland_surface(&surface_desc, allocation_callbacks)
+        }
+
+        #[cfg(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd",
+            target_os = "openbsd"
+        ))]
+        RawWindowHandle::Xlib(handle) => {
+            let surface_desc = vk::XlibSurfaceCreateInfoKHR::builder()
+                .dpy(handle.display as *mut _)
+                .window(handle.window);
+            let surface_fn = khr::XlibSurface::new(entry, instance);
+            surface_fn.create_xlib_surface(&surface_desc, allocation_callbacks)
+        }
+
+        #[cfg(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd",
+            target_os = "openbsd"
+        ))]
+        RawWindowHandle::Xcb(handle) => {
+            let surface_desc = vk::XcbSurfaceCreateInfoKHR::builder()
+                .connection(handle.connection as *mut _)
+                .window(handle.window);
+            let surface_fn = khr::XcbSurface::new(entry, instance);
+            surface_fn.create_xcb_surface(&surface_desc, allocation_callbacks)
+        }
+
+        #[cfg(any(target_os = "android"))]
+        RawWindowHandle::Android(handle) => {
+            let surface_desc =
+                vk::AndroidSurfaceCreateInfoKHR::builder().window(handle.a_native_window as _);
+            let surface_fn = khr::AndroidSurface::new(entry, instance);
+            surface_fn.create_android_surface(&surface_desc, allocation_callbacks)
+        }
+
+        #[cfg(any(target_os = "macos"))]
+        RawWindowHandle::MacOS(handle) => {
+            use raw_window_metal::{macos, Layer};
+
+            let layer = match macos::metal_layer_from_handle(handle) {
+                Layer::Existing(layer) | Layer::Allocated(layer) => layer as *mut _,
+                Layer::None => return Err(vk::Result::ERROR_INITIALIZATION_FAILED)
+            };
+
+            let surface_desc = vk::MetalSurfaceCreateInfoEXT::builder().layer(&*layer);
+            let surface_fn = ext::MetalSurface::new(entry, instance);
+            surface_fn.create_metal_surface(&surface_desc, allocation_callbacks)
+        }
+
+        #[cfg(any(target_os = "ios"))]
+        RawWindowHandle::IOS(handle) => {
+            use raw_window_metal::{ios, Layer};
+
+            let layer = match ios::metal_layer_from_handle(handle) {
+                Layer::Existing(layer) | Layer::Allocated(layer) => layer as *mut _,
+                Layer::None => return Err(vk::Result::ERROR_INITIALIZATION_FAILED)
+            };
+
+            let surface_desc = vk::MetalSurfaceCreateInfoEXT::builder().layer(&*layer);
+            let surface_fn = ext::MetalSurface::new(entry, instance);
+            surface_fn.create_metal_surface(&surface_desc, allocation_callbacks)
+        }
+
+        _ => Err(vk::Result::ERROR_EXTENSION_NOT_PRESENT), // not supported
+    }
+}
+
+/// Query the required instance extensions for creating a surface from a window handle.
+///
+/// The returned extensions will include all extension dependencies.
+pub fn enumerate_required_extensions(
+    window_handle: &dyn HasRawWindowHandle,
+) -> VkResult<Vec<&'static CStr>> {
+    let extensions = match window_handle.raw_window_handle() {
+        #[cfg(target_os = "windows")]
+        RawWindowHandle::Windows(_) => vec![khr::Surface::name(), khr::Win32Surface::name()],
+
+        #[cfg(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd",
+            target_os = "openbsd"
+        ))]
+        RawWindowHandle::Wayland(_) => vec![khr::Surface::name(), khr::WaylandSurface::name()],
+
+        #[cfg(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd",
+            target_os = "openbsd"
+        ))]
+        RawWindowHandle::Xlib(_) => vec![khr::Surface::name(), khr::XlibSurface::name()],
+
+        #[cfg(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd",
+            target_os = "openbsd"
+        ))]
+        RawWindowHandle::Xcb(_) => vec![khr::Surface::name(), khr::XcbSurface::name()],
+
+        #[cfg(any(target_os = "android"))]
+        RawWindowHandle::Android(_) => vec![khr::Surface::name(), khr::AndroidSurface::name()],
+
+        #[cfg(any(target_os = "macos"))]
+        RawWindowHandle::MacOS(_) => vec![khr::Surface::name(), ext::MetalSurface::name()],
+
+        #[cfg(any(target_os = "ios"))]
+        RawWindowHandle::IOS(_) => vec![khr::Surface::name(), ext::MetalSurface::name()],
+
+        _ => return Err(vk::Result::ERROR_EXTENSION_NOT_PRESENT),
+    };
+
+    Ok(extensions)
+}
