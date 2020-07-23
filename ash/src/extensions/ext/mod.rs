4049cb3c3cd4351be68baffdc4bcba7edfa51b14
Apoorva Joshi <apoorvaj@apoorvaj.io>
2020-07-24 00:55:40 +0200

    Deprecate the DebugMarker and DebugReport extension modules

--- ash/src/extensions/ext/mod.rs
+++ ash/src/extensions/ext/mod.rs
@@ -4,7 +4,9 @@ pub use self::debug_utils::DebugUtils;
 pub use self::metal_surface::MetalSurface;
 pub use self::tooling_info::ToolingInfo;
 
+#[deprecated(note = "Please use the [DebugUtils](struct.DebugUtils.html) extension instead.")]
 mod debug_marker;
+#[deprecated(note = "Please use the [DebugUtils](struct.DebugUtils.html) extension instead.")]
 mod debug_report;
 mod debug_utils;
 mod metal_surface;
