ash-rs/ash

commit c6d5d66142ff58a2f2d8b1561117e2d3a01f5596

entry_libloading: Provide Vulkan library loader from custom path (#319)
It is in some cases necessary to load a specific Vulkan dll/so with a
different name and/or from a different location.

Instead of copying this function to a downstream project (and making the
LoadingError constructor public to retain the same interface), split
Entry::new() such that downstream projects can easily specify and use an
alternate Vulkan implementation.

ash/src/entry_libloading.rs
@@ -1,6 +1,7 @@
 use crate::entry::EntryCustom;
 use libloading::Library;
 use std::error::Error;
+use std::ffi::OsStr;
 use std::fmt;
 use std::ptr;
 use std::sync::Arc;
@@ -39,6 +40,8 @@ impl Error for LoadingError {
 }
 
 impl EntryCustom<Arc<Library>> {
+    /// Load default Vulkan library for the current platform
+    ///
     /// ```rust,no_run
     /// use ash::{vk, Entry, version::EntryV1_0};
     /// # fn main() -> Result<(), Box<std::error::Error>> {
@@ -55,9 +58,12 @@ impl EntryCustom<Arc<Library>> {
     /// # Ok(()) }
     /// ```
     pub fn new() -> Result<Entry, LoadingError> {
-        let lib = Library::new(&LIB_PATH)
-            .map_err(LoadingError)
-            .map(Arc::new)?;
+        Self::with_library(&LIB_PATH)
+    }
+
+    /// Load Vulkan library at `path`
+    pub fn with_library<P: AsRef<OsStr>>(path: &P) -> Result<Entry, LoadingError> {
+        let lib = Library::new(path).map_err(LoadingError).map(Arc::new)?;
 
         Ok(Self::new_custom(lib, |vk_lib, name| unsafe {
             vk_lib

commit f7318f3669a48971f1c1e2b21bae160af2b7f740

entry_libloading: Do not pass AsRef implementation by reference
All type parameters are implicitly bound by `Sized`; matching the `&`
out of `&str` implies that `str` must be bound by `Sized` which is not
true, resulting in:

    18 |         let entry = ash::Entry::with_library("foo")?;
       |                                              ^^^^^ doesn't have a size known at compile-time
       |
      ::: /home/marijn/Code/TraverseResearch/ash/ash/src/entry_libloading.rs:73:39
       |
    73 |     pub unsafe fn with_library(path: &impl AsRef<OsStr>) -> Result<Entry, LoadingError> {
       |                                       ----------------- required by this bound in `ash::entry_libloading::<impl EntryCustom<Arc<libloading::Library>>>::with_library`

After all `Library::new` accepts an `impl AsRef<OsStr>` without borrow,
which is what this function is modeled after to pass the same parameter
on directly.

Fixes: c6d5d66 ("entry_libloading: Provide Vulkan library loader from custom path (#319)")

ash/src/entry_libloading.rs
@@ -62,7 +62,7 @@ impl EntryCustom<Arc<Library>> {
     /// # Ok(()) }
     /// ```
     pub unsafe fn new() -> Result<Entry, LoadingError> {
-        Self::with_library(&LIB_PATH)
+        Self::with_library(LIB_PATH)
     }
 
     /// Load Vulkan library at `path`
@@ -70,7 +70,7 @@ impl EntryCustom<Arc<Library>> {
     /// # Safety
     /// `dlopen`ing native libraries is inherently unsafe. The safety guidelines
     /// for [`Library::new`] and [`Library::get`] apply here.
-    pub unsafe fn with_library(path: &impl AsRef<OsStr>) -> Result<Entry, LoadingError> {
+    pub unsafe fn with_library(path: impl AsRef<OsStr>) -> Result<Entry, LoadingError> {
         let lib = Library::new(path).map_err(LoadingError).map(Arc::new)?;
 
         Ok(Self::new_custom(lib, |vk_lib, name| {