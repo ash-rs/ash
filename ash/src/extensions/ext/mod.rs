bbbcd037a9cb29be9f54212eaee30551beaab0da
Rua <ruawhitepaw@gmail.com>
2021-03-20 14:08:00 +0100

    Add VK_EXT_full_screen_exclusive extension (#399)

--- ash/src/extensions/ext/mod.rs
+++ ash/src/extensions/ext/mod.rs
@@ -4,6 +4,7 @@ pub use self::debug_marker::DebugMarker;
 #[allow(deprecated)]
 pub use self::debug_report::DebugReport;
 pub use self::debug_utils::DebugUtils;
+pub use self::full_screen_exclusive::FullScreenExclusive;
 pub use self::metal_surface::MetalSurface;
 pub use self::tooling_info::ToolingInfo;
 
@@ -13,5 +14,6 @@ mod debug_marker;
 #[deprecated(note = "Please use the [DebugUtils](struct.DebugUtils.html) extension instead.")]
 mod debug_report;
 mod debug_utils;
+mod full_screen_exclusive;
 mod metal_surface;
 mod tooling_info;
