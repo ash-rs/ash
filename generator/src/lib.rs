ash-rs/ash

commit 215511f9cf2e95d5f112da72197b7248b562a64d

Implement ExtendXXX for multiple root create infos if there are more than 1

generator/src/lib.rs
@@ -1583,16 +1583,20 @@ pub fn derive_setters(
 
     let extends_name = name_to_tokens(&format!("Extends{}", name));
 
-    let next_extends = _struct.extends.as_ref().map(|extends| {
-        let extends = extends
-            .split(',')
-            .find(|extend| root_create_info_names.contains(&extend.to_string()))
-            .expect("Should have a root create info");
-        name_to_tokens(&format!("Extends{}", name_to_tokens(&extends)))
-    });
+    let root_extends: Vec<Ident> = _struct
+        .extends
+        .as_ref()
+        .map(|extends| {
+            extends
+                .split(',')
+                .filter(|extend| root_create_info_names.contains(&extend.to_string()))
+                .map(|extends| name_to_tokens(&format!("Extends{}", name_to_tokens(&extends))))
+                .collect()
+        })
+        .unwrap_or(vec![]);
 
     // We only implement a next methods for root create infos
-    let next_function = if has_next && next_extends.is_none() {
+    let next_function = if has_next && root_extends.is_empty() {
         quote! {
             /// Prepends the given extension struct between the root and the first pointer. This
             /// method only exists on create infos that can be passed to a function directly. Only
@@ -1624,16 +1628,14 @@ pub fn derive_setters(
     };
 
     // If the struct extends something we need to implement the root create info trait.
-    let impl_extend_trait = if let Some(next_extends) = next_extends {
+    let impl_extend_trait = root_extends.iter().map(|extends| {
         quote! {
-            unsafe impl #next_extends for #name_builder<'_> {
+            unsafe impl #extends for #name_builder<'_> {
             }
-            unsafe impl #next_extends for #name {
+            unsafe impl #extends for #name {
             }
         }
-    } else {
-        quote! {}
-    };
+    });
 
     let q = quote! {
         impl #name {
@@ -1651,7 +1653,7 @@ pub fn derive_setters(
             marker: ::std::marker::PhantomData<&'a ()>,
         }
 
-        #impl_extend_trait
+        #(#impl_extend_trait)*
         #next_trait
 
 

commit 1986bf6119590f154ee12b4c8108957763578b9d

generator: Use the same predicate for push_next and its traits
Some structs turn out to be root structs when their name is not in the
`root_structs` set, even when they themselves extend another struct.
This was already taken care of for `push_next` which is emitted in this
situation, but the trait referenced by `push_next`'s `T` bound is still
relying on the `extends` field to not exist in `vk.xml` at all, leading
to it not being generated despite `push_next` needing it.

Fixes: 215511f ("Implement ExtendXXX for multiple root create infos if
there are more than 1")

generator/src/lib.rs
@@ -1959,7 +1959,7 @@ pub fn derive_setters(
         })
     });
 
-    let extends_name = name_to_tokens(&format!("Extends{}", name));
+    let extends_name = format_ident!("Extends{}", name);
 
     let root_structs: Vec<Ident> = _struct
         .extends
@@ -1968,13 +1968,15 @@ pub fn derive_setters(
             extends
                 .split(',')
                 .filter(|extend| root_struct_names.contains(*extend))
-                .map(|extends| name_to_tokens(&format!("Extends{}", name_to_tokens(&extends))))
+                .map(|extends| format_ident!("Extends{}", name_to_tokens(extends)))
                 .collect()
         })
         .unwrap_or_else(Vec::new);
 
+    let is_root_struct = has_next && root_structs.is_empty();
+
     // We only implement a next methods for root structs with a `pnext` field.
-    let next_function = if has_next && root_structs.is_empty() {
+    let next_function = if is_root_struct {
         quote! {
             /// Prepends the given extension struct between the root and the first pointer. This
             /// method only exists on structs that can be passed to a function directly. Only
@@ -2001,27 +2003,22 @@ pub fn derive_setters(
             }
         }
     } else {
-        quote! {}
+        quote!()
     };
 
-    // Root structs come with their own trait that structs that extends this struct will
-    // implement
-    let next_trait = if has_next && _struct.extends.is_none() {
-        quote! {
-            pub unsafe trait #extends_name {
-            }
-        }
+    // Root structs come with their own trait that structs that extend
+    // this struct will implement
+    let next_trait = if is_root_struct {
+        quote!(pub unsafe trait #extends_name {})
     } else {
-        quote! {}
+        quote!()
     };
 
     // If the struct extends something we need to implement the trait.
     let impl_extend_trait = root_structs.iter().map(|extends| {
         quote! {
-            unsafe impl #extends for #name_builder<'_> {
-            }
-            unsafe impl #extends for #name {
-            }
+            unsafe impl #extends for #name_builder<'_> {}
+            unsafe impl #extends for #name {}
         }
     });
 