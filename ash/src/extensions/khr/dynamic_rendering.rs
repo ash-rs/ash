6857f7b4d3550209622ae1b8742d81e8a05aad40
Marijn Suijten <marijn@traverseresearch.nl>
2021-11-09 22:49:41 +0100

    extensions/khr: Add VK_KHR_dynamic_rendering wrapper (#488)

---
+++ ash/src/extensions/khr/dynamic_rendering.rs
@@ -0,0 +1,43 @@
+use crate::vk;
+use crate::{Device, Instance};
+use std::ffi::CStr;
+use std::mem;
+
+#[derive(Clone)]
+pub struct DynamicRendering {
+    fns: vk::KhrDynamicRenderingFn,
+}
+
+impl DynamicRendering {
+    pub fn new(instance: &Instance, device: &Device) -> Self {
+        let dynamic_rendering_fn = vk::KhrDynamicRenderingFn::load(|name| unsafe {
+            mem::transmute(instance.get_device_proc_addr(device.handle(), name.as_ptr()))
+        });
+        Self {
+            fns: dynamic_rendering_fn,
+        }
+    }
+
+    pub fn name() -> &'static CStr {
+        vk::KhrDynamicRenderingFn::name()
+    }
+
+    pub fn fp(&self) -> &vk::KhrDynamicRenderingFn {
+        &self.fns
+    }
+
+    #[doc = "<https://www.khronos.org/registry/vulkan/specs/1.2-extensions/man/html/vkCmdBeginRenderingKHR.html>"]
+    pub unsafe fn cmd_begin_rendering(
+        &self,
+        command_buffer: vk::CommandBuffer,
+        rendering_info: &vk::RenderingInfoKHR,
+    ) {
+        self.fns
+            .cmd_begin_rendering_khr(command_buffer, rendering_info)
+    }
+
+    #[doc = "<https://www.khronos.org/registry/vulkan/specs/1.2-extensions/man/html/vkCmdEndRenderingKHR.html>"]
+    pub unsafe fn cmd_end_rendering(&self, command_buffer: vk::CommandBuffer) {
+        self.fns.cmd_end_rendering_khr(command_buffer)
+    }
+}
