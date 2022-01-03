adf0338ed83c5badad346e3a13ee848bd13b975a
Markus Siglreithmaier <m.siglreith@gmail.com>
2022-01-03 23:24:36 +0100

    example: Refactor event loop handling for continuous redraw (#542)
    Switch to continuous redrawing using `Poll` and `MainEventsCleared`.
    
    Fixes hangs in case of non-Mailbox present modes.

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -184,24 +184,24 @@ impl ExampleBase {
         self.event_loop
             .borrow_mut()
             .run_return(|event, _, control_flow| {
-                *control_flow = ControlFlow::Wait;
-                f();
-                if let Event::WindowEvent {
-                    event:
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        },
-                    ..
-                } = event
-                {
-                    *control_flow = ControlFlow::Exit
+                *control_flow = ControlFlow::Poll;
+                match event {
+                    Event::WindowEvent {
+                        event:
+                            WindowEvent::CloseRequested
+                            | WindowEvent::KeyboardInput {
+                                input:
+                                    KeyboardInput {
+                                        state: ElementState::Pressed,
+                                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                                        ..
+                                    },
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    Event::MainEventsCleared => f(),
+                    _ => (),
                 }
             });
     }
