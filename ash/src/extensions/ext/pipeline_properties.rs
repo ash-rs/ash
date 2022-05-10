77bed8c5f27145047cac5055c65ce4175b6743c7
Marijn Suijten <marijn@traverseresearch.nl>
2022-05-10 15:18:13 +0200

    extensions/ext: Add VK_EXT_pipeline_properties device extension

---
+++ ash/src/extensions/ext/pipeline_properties.rs
@@ -0,0 +1,47 @@
+use crate::prelude::*;
+use crate::vk;
+use crate::{Device, Instance};
+use std::ffi::CStr;
+use std::mem;
+
+/// <https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_EXT_pipeline_properties.html>
+#[derive(Clone)]
+pub struct PipelineProperties {
+    handle: vk::Device,
+    fp: vk::ExtPipelinePropertiesFn,
+}
+
+impl PipelineProperties {
+    pub fn new(instance: &Instance, device: &Device) -> Self {
+        let handle = device.handle();
+        let fp = vk::ExtPipelinePropertiesFn::load(|name| unsafe {
+            mem::transmute(instance.get_device_proc_addr(handle, name.as_ptr()))
+        });
+        Self { handle, fp }
+    }
+
+    /// <https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/vkGetPipelinePropertiesEXT.html>
+    ///
+    /// Currently only accepts [`vk::PipelinePropertiesIdentifierEXT`].
+    #[inline]
+    pub unsafe fn get_pipeline_properties(
+        &self,
+        pipeline_info: &vk::PipelineInfoEXT,
+        pipeline_properties: *mut vk::BaseOutStructure,
+    ) -> VkResult<()> {
+        (self.fp.get_pipeline_properties_ext)(self.handle, pipeline_info, pipeline_properties)
+            .result()
+    }
+
+    pub const NAME: &'static CStr = vk::ExtPipelinePropertiesFn::NAME;
+
+    #[inline]
+    pub fn fp(&self) -> &vk::ExtPipelinePropertiesFn {
+        &self.fp
+    }
+
+    #[inline]
+    pub fn device(&self) -> vk::Device {
+        self.handle
+    }
+}
