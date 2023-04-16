9380d15ccbe2744ce88e0244697a5d48cacb1bc7
Marijn Suijten <marijn@traverseresearch.nl>
2023-04-16 22:48:59 +0200

    extensions/khr: Take the remaining `p_next`-containing structs as `&mut`
    Version 2 of `get_physical_device_surface_capabilities` and the matching
    `vk::SurfaceCapabilitiesKHR` struct exist solely to provide an `sType`
    and `pNext` field to allow extending the original query with additional
    data via extension structs.  However, this API when introduced in #530
    only returns the `default()`-initialized struct making it just as
    constrained as `get_physical_device_surface_capabilities()`.  Solve this
    by taking `vk::SurfaceCapabilities2KHR` as `&mut` just like any similar
    API.
    
    And just like this, do the same for the remaining:
    - `AccelerationStructure::get_acceleration_structure_build_sizes()`
    - `ExternalMemoryFd::get_memory_fd_properties()`
    - `ExternalMemoryWin32::get_memory_win32_handle_properties()`
    
    In case these structs get extended somewhere down the line, which the
    Vulkan API allows for.

--- ash/src/extensions/khr/external_memory_fd.rs
+++ ash/src/extensions/khr/external_memory_fd.rs
@@ -4,6 +4,7 @@ use crate::{Device, Instance};
 use std::ffi::CStr;
 use std::mem;
 
+/// <https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/VK_KHR_external_memory_fd.html>
 #[derive(Clone)]
 pub struct ExternalMemoryFd {
     handle: vk::Device,
@@ -32,15 +33,10 @@ impl ExternalMemoryFd {
         &self,
         handle_type: vk::ExternalMemoryHandleTypeFlags,
         fd: i32,
-    ) -> VkResult<vk::MemoryFdPropertiesKHR> {
-        let mut memory_fd_properties = Default::default();
-        (self.fp.get_memory_fd_properties_khr)(
-            self.handle,
-            handle_type,
-            fd,
-            &mut memory_fd_properties,
-        )
-        .result_with_success(memory_fd_properties)
+        memory_fd_properties: &mut vk::MemoryFdPropertiesKHR,
+    ) -> VkResult<()> {
+        (self.fp.get_memory_fd_properties_khr)(self.handle, handle_type, fd, memory_fd_properties)
+            .result()
     }
 
     pub const NAME: &'static CStr = vk::KhrExternalMemoryFdFn::NAME;
