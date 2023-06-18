e8fee263616349106a74ad1c8f6eb20f0bdd231f
BeastLe9enD <beastle9end@outlook.de>
2023-06-18 12:27:44 +0200

    ✨ Add support for VK_NV_memory_decompression

--- ash/src/extensions/nv/mod.rs
+++ ash/src/extensions/nv/mod.rs
@@ -1,9 +1,11 @@
 pub use self::coverage_reduction_mode::CoverageReductionMode;
 pub use self::device_diagnostic_checkpoints::DeviceDiagnosticCheckpoints;
+pub use self::memory_decompression::MemoryDecompression;
 pub use self::mesh_shader::MeshShader;
 pub use self::ray_tracing::RayTracing;
 
 mod coverage_reduction_mode;
 mod device_diagnostic_checkpoints;
+mod memory_decompression;
 mod mesh_shader;
 mod ray_tracing;
