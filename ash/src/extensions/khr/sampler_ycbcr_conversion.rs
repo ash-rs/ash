269200999f2c9a22b2e4e4cb789f99fcfff3ee5a
Chris Spencer <spencercw@gmail.com>
2023-08-14 14:39:19 +0100

    extensions/khr: Add VK_KHR_sampler_ycbcr_conversion

---
+++ ash/src/extensions/khr/sampler_ycbcr_conversion.rs
@@ -0,0 +1,66 @@
+use crate::prelude::*;
+use crate::vk;
+use crate::RawPtr;
+use crate::{Device, Instance};
+use std::ffi::CStr;
+use std::mem;
+
+/// <https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_KHR_sampler_ycbcr_conversion.html>
+#[derive(Clone)]
+pub struct SamplerYcbcrConversion {
+    handle: vk::Device,
+    fp: vk::KhrSamplerYcbcrConversionFn,
+}
+
+impl SamplerYcbcrConversion {
+    pub fn new(instance: &Instance, device: &Device) -> Self {
+        let handle = device.handle();
+        let fp = vk::KhrSamplerYcbcrConversionFn::load(|name| unsafe {
+            mem::transmute(instance.get_device_proc_addr(handle, name.as_ptr()))
+        });
+        Self { handle, fp }
+    }
+
+    /// <https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/vkCreateSamplerYcbcrConversion.html>
+    #[inline]
+    pub unsafe fn create_sampler_ycbcr_conversion(
+        &self,
+        create_info: &vk::SamplerYcbcrConversionCreateInfo,
+        allocation_callbacks: Option<&vk::AllocationCallbacks>,
+    ) -> VkResult<vk::SamplerYcbcrConversion> {
+        let mut ycbcr_conversion = mem::zeroed();
+        (self.fp.create_sampler_ycbcr_conversion_khr)(
+            self.handle,
+            create_info,
+            allocation_callbacks.as_raw_ptr(),
+            &mut ycbcr_conversion,
+        )
+        .result_with_success(ycbcr_conversion)
+    }
+
+    /// <https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/vkDestroySamplerYcbcrConversion.html>
+    #[inline]
+    pub unsafe fn destroy_sampler_ycbcr_conversion(
+        &self,
+        ycbcr_conversion: vk::SamplerYcbcrConversion,
+        allocation_callbacks: Option<&vk::AllocationCallbacks>,
+    ) {
+        (self.fp.destroy_sampler_ycbcr_conversion_khr)(
+            self.handle,
+            ycbcr_conversion,
+            allocation_callbacks.as_raw_ptr(),
+        )
+    }
+
+    pub const NAME: &'static CStr = vk::KhrSamplerYcbcrConversionFn::NAME;
+
+    #[inline]
+    pub fn fp(&self) -> &vk::KhrSamplerYcbcrConversionFn {
+        &self.fp
+    }
+
+    #[inline]
+    pub fn device(&self) -> vk::Device {
+        self.handle
+    }
+}
