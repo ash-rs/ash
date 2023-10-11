d51682b87e60ca94ea785e4b85c3778c2806b26e
Marijn Suijten <marijn@traverseresearch.nl>
2023-10-11 20:24:42 +0200

    platform_types: Convert Windows `HANDLE` types to `isize`
    The `windows` crate treats these as `isize` rather than raw void
    pointers:
    https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/Foundation/struct.HWND.html
    
    And `raw-window-handle 0.6` recently started to do the same:
    https://github.com/rust-windowing/raw-window-handle/pull/136
    
    However, the win32 documentation still states that these should be
    `PVOID`:
    https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types

--- ash/src/vk/platform_types.rs
+++ ash/src/vk/platform_types.rs
@@ -10,12 +10,16 @@ pub type xcb_window_t = u32;
 pub type xcb_visualid_t = u32;
 pub type MirConnection = *const c_void;
 pub type MirSurface = *const c_void;
-pub type HINSTANCE = *const c_void;
-pub type HWND = *const c_void;
+/// <https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/Foundation/struct.HANDLE.html>
+pub type HANDLE = isize;
+/// <https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/Foundation/struct.HINSTANCE.html>
+pub type HINSTANCE = HANDLE;
+/// <https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/Foundation/struct.HWND.html>
+pub type HWND = HANDLE;
+/// <https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/Graphics/Gdi/struct.HMONITOR.html>
+pub type HMONITOR = HANDLE;
 pub type wl_display = c_void;
 pub type wl_surface = c_void;
-pub type HANDLE = *mut c_void;
-pub type HMONITOR = HANDLE;
 pub type DWORD = c_ulong;
 pub type LPCWSTR = *const u16;
 pub type zx_handle_t = u32;
