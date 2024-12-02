7a61d3f480f51b8f989b8d5f86292a249d3ac17a
Marijn Suijten <marijns95@gmail.com>
2024-12-02 18:01:45 +0100

    extensions: Remove `read_into_defaulted_vector()` to let caller pass `pNext`
    We've often discussed `pNext` chains in output arrays (i.e. #465, #588,
     #744), and I always wondered why `read_into_defaulted_vector()` still
    existed: turns out this helper function was only hiding a few more
    remaining cases where the caller was previously _not_ able to manually
    extend the `pNext` chain to request arbitrary more structures to be
    filled with information.
    
    Replace these remaining cases with a separate `_len()` getter, and have
    the main function take a caller-allocated `&mut [T]` slice where they
    can initialize the target struct including `pNext` pointer chains.

--- ash/src/prelude.rs
+++ ash/src/prelude.rs
@@ -64,43 +64,6 @@ where
     }
 }
 
-/// Repeatedly calls `f` until it does not return [`vk::Result::INCOMPLETE`] anymore, ensuring all
-/// available data has been read into the vector.
-///
-/// Items in the target vector are [`default()`][Default::default()]-initialized which is required
-/// for [`vk::BaseOutStructure`]-like structs where [`vk::BaseOutStructure::s_type`] needs to be a
-/// valid type and [`vk::BaseOutStructure::p_next`] a valid or [`null`][ptr::null_mut()]
-/// pointer.
-///
-/// See for example [`vkEnumerateInstanceExtensionProperties`]: the number of available items may
-/// change between calls; [`vk::Result::INCOMPLETE`] is returned when the count increased (and the
-/// vector is not large enough after querying the initial size), requiring Ash to try again.
-///
-/// [`vkEnumerateInstanceExtensionProperties`]: https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/vkEnumerateInstanceExtensionProperties.html
-pub(crate) unsafe fn read_into_defaulted_vector<
-    N: Copy + Default + TryInto<usize>,
-    T: Default + Clone,
->(
-    f: impl Fn(&mut N, *mut T) -> vk::Result,
-) -> VkResult<Vec<T>>
-where
-    <N as TryInto<usize>>::Error: core::fmt::Debug,
-{
-    loop {
-        let mut count = N::default();
-        f(&mut count, ptr::null_mut()).result()?;
-        let mut data = alloc::vec![Default::default(); count.try_into().expect("`N` failed to convert to `usize`")];
-
-        let err_code = f(&mut count, data.as_mut_ptr());
-        if err_code != vk::Result::INCOMPLETE {
-            break err_code.set_vec_len_on_success(
-                data,
-                count.try_into().expect("`N` failed to convert to `usize`"),
-            );
-        }
-    }
-}
-
 #[cfg(feature = "debug")]
 pub(crate) fn debug_flags<Value: Into<u64> + Copy>(
     f: &mut core::fmt::Formatter<'_>,
