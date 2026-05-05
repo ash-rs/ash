03f40040b522ac9bb6efe429136ec37a09354e15
Nils Hasenbanck <hasenbanck@users.noreply.github.com>
2026-05-05 11:26:28 +0200

    extensions/khr: Add VK_KHR_present_wait2 (#1026)
    Co-authored-by: Marijn Suijten <marijn@traverseresearch.nl>

---
+++ ash/src/extensions/khr/present_wait2.rs
@@ -0,0 +1,16 @@
+//! <https://docs.vulkan.org/refpages/latest/refpages/source/VK_KHR_present_wait2.html>
+
+use crate::vk;
+use crate::VkResult;
+
+impl crate::khr::present_wait2::Device {
+    /// <https://docs.vulkan.org/refpages/latest/refpages/source/vkWaitForPresent2KHR.html>
+    #[inline]
+    pub unsafe fn wait_for_present2(
+        &self,
+        swapchain: vk::SwapchainKHR,
+        present_wait2_info: &vk::PresentWait2InfoKHR<'_>,
+    ) -> VkResult<()> {
+        (self.fp.wait_for_present2_khr)(self.handle, swapchain, present_wait2_info).result()
+    }
+}
